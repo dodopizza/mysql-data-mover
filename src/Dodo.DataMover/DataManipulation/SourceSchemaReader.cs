ca36096d5f4419987607011081926a36931b41aa
Evgeniy Drenkov <e.drenkov@dodopizza.com>
2021-07-28 15:27:58 +0300

    GetLimitOverride refactoring

--- src/Dodo.DataMover/DataManipulation/SourceSchemaReader.cs
+++ src/Dodo.DataMover/DataManipulation/SourceSchemaReader.cs
@@ -79,11 +79,7 @@ namespace Dodo.DataMover.DataManipulation
                 yield break;
             }
 
-            long? limitOverride = _dataMoverSettings.LimitOverrides
-                .Where(entry => Regex.IsMatch(tableName, entry.Key))
-                .Select(entry => entry.Value)
-                .FirstOrDefault();
-            long? limit = limitOverride ?? _dataMoverSettings.Limit;
+            long? limit = GetLimitOverride(tableName) ?? _dataMoverSettings.Limit;
             foreach (var currentBatchSize in GetBatchSizes(limit, _dataMoverSettings.ReadBatchSize))
             {
                 if (ct.IsCancellationRequested)
@@ -105,6 +101,12 @@ namespace Dodo.DataMover.DataManipulation
                 }
             }
         }
+
+        private long? GetLimitOverride(string tableName) =>
+            _dataMoverSettings.LimitOverrides
+                .Where(entry => Regex.IsMatch(tableName, entry.Key))
+                .Select(entry => entry.Value)
+                .FirstOrDefault();
 
         string GenerateKeyRangeSelectSql(string tableName, List<object> from, List<string> pkNames)
         {
